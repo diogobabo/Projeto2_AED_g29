a01539522d18182949bca76f8ca144e3fafe078d
DiogoBabo <diogo.babo@gmail.com>
2022-01-18 14:28:58 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/Projeto2_AED.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto2_AED.dir/main.cpp.obj"
+  "Projeto2_AED.exe"
+  "Projeto2_AED.exe.manifest"
+  "Projeto2_AED.pdb"
+  "libProjeto2_AED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projeto2_AED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
