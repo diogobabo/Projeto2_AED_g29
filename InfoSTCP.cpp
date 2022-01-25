06ae569e88f918547e44e1043baa22a29addae2c
diogobabo <diogo.babo@gmail.com>
2022-01-25 22:14:36 +0000

    Stops

---
+++ InfoSTCP.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Diogo Babo on 25/01/2022.
+//
+
+#include "InfoSTCP.h"
---
+++ InfoSTCP.h
@@ -0,0 +1,14 @@
+//
+// Created by Diogo Babo on 25/01/2022.
+//
+
+#ifndef PROJETO2_AED_INFOSTCP_H
+#define PROJETO2_AED_INFOSTCP_H
+
+
+class InfoSTCP {
+
+};
+
+
+#endif //PROJETO2_AED_INFOSTCP_H
